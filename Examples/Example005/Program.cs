848e49b23b5ddf7459451fea25f459ae2cea848f
Dmitry <ya.potapov@yandex.ru>
2022-07-10 19:40:33 +0300

    Exampe005 weight

---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2;
+int b = 1;
+int c = 6;
+int d = 8;
+int e = 4;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
