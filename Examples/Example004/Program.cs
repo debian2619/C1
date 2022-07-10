56b0902e1e5a094b4a95e835bcaf3b0874e55ed6
Dmitry <ya.potapov@yandex.ru>
2022-07-10 18:32:06 +0300

    Masha

---
+++ Examples/Example004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Put username ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("yaooo..the Masha ");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
