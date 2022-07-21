ac4b410a4168cf2b51526612ce607517338124dd
Dmitry <ya.potapov@yandex.ru>
2022-07-21 17:18:13 +0300

    created esersuse10

---
+++ Examples/Esersise10/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {15,26,37,48,56,66,77,66,97};
+int n = array.Length;
+int find = 66;
+int index= 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+       
+    }
+    index++;  
+}
+
+
